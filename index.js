846ce3edb640ea0396fa07f5b47724518712f06f
shnakamura <joaovtnakamura@gmail.com>
2023-11-23 16:53:12 -0300

    Reset scroll

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-let inputDown = false;
-let startX;
-let scrollLeft;
+let isInputPressed = false;
+
+let scrollStart = 0;
+let scrollAmount = 0;
 
 const track = document.getElementById("image-track")
 
+const refresh = () => {
+    isInputPressed = false;
+
+    scrollStart = 0;
+    scrollAmount = 0;
+
+    track.scrollLeft = scrollAmount;
+}
+
 const onInputPress = event => {
-    inputDown = true;
-    startX = event.pageX - track.offsetLeft;
-    scrollLeft = track.scrollLeft;
+    isInputPressed = true;
+
+    scrollStart = event.pageX - track.offsetLeft;
+    scrollAmount = track.scrollLeft;
 }
 
 const onInputRelease = event => {
-    inputDown = false;
+    isInputPressed = false;
 }
 
 const onInputMove = event => {
     event.preventDefault();
 
-    if (!inputDown) { 
+    if (!isInputPressed) { 
         return; 
     }
     
     const difference = event.pageX - track.offsetLeft;
-    const scroll = difference - startX;
+    const scroll = difference - scrollStart;
 
-    track.scrollLeft = scrollLeft - scroll;
+    track.scrollLeft = scrollAmount - scroll;
 }
+
+window.onload = refresh();
 
 window.onmouseleave = event => onInputRelease(event);
 window.onmouseup = event => onInputRelease(event);
@@ -34,5 +47,5 @@ window.onmousemove = event => onInputMove(event);
 
 window.ontouchstart = event => onInputPress(event.touches[0]);
 window.ontouchmove = event => onInputMove(event.touches[0]);
-window.ontouchend = event => onInputRelease(event.touches[0]);
+window.ontouchend = event => onInputRelease(event.touches);
 window.ontouchcancel = event => onInputRelease(event.touches[0]);
