e9f559fe2b4429d0ee6706406a15f1122c682283
shnakamura <joaovtnakamura@gmail.com>
2023-11-23 23:18:26 -0300

    Push

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let isInputPressed = false;
+const tracker = document.getElementById("shirt-tracker");
 
-let scrollStart = 0;
-let scrollAmount = 0;
+const colorNames = [
+    "black", "white", "gray", "sky-blue", "offwhite",
+    "wine", "maple", "petal-pink", "lilac", "violet",
+    "ocean-green", "sea-green", "cyan", "dark-blue",
+    "lead", "sand"
+];
 
-const track = document.getElementById("image-track")
+let currentStamp = 0;
 
 const moveTrackerTo = (name) => {
     const element = document.getElementById(name);
     const gap = 8;
     const padding = 8;
 
-    track.scrollLeft = element.offsetLeft - padding - gap;
+    tracker.scrollLeft = element.offsetLeft - padding - gap;
+}
+
+const changeStamp = () => {
+    if (currentStamp >= 3) {
+        currentStamp = 0;
+    }
+
+    for (let i = 0; i < 13; i++) {
+        const stamp = document.getElementById(`stamp-${i}`);
+
+        stamp.src = `assets/img/black-stamp-${currentStamp}.png`;
+    }
+
+    currentStamp++;
 }
 
 const refresh = () => {
@@ -19,41 +37,7 @@ const refresh = () => {
     scrollStart = 0;
     scrollAmount = 0;
 
-    track.scrollLeft = scrollAmount;
-}
-
-const onInputPress = event => {
-    isInputPressed = true;
-
-    scrollStart = event.pageX - track.offsetLeft;
-    scrollAmount = track.scrollLeft;
-}
-
-const onInputRelease = () => {
-    isInputPressed = false;
-}
-
-const onInputMove = event => {
-    event.preventDefault();
-
-    if (!isInputPressed) { 
-        return; 
-    }
-    
-    const difference = event.pageX - track.offsetLeft;
-    const scroll = difference - scrollStart;
-
-    track.scrollLeft = scrollAmount - scroll;
+    tracker.scrollLeft = scrollAmount;
 }
 
 window.onload = refresh();
-
-window.onmouseleave = event => onInputRelease(event);
-window.onmouseup = () => onInputRelease();
-window.onmousedown = event => onInputPress(event);
-window.onmousemove = event => onInputMove(event);
-
-window.ontouchstart = event => onInputPress(event.touches[0]);
-window.ontouchmove = event => onInputMove(event.touches[0]);
-window.ontouchend = () => onInputRelease();
-window.ontouchcancel = event => onInputRelease(event.touches[0]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,19 @@ module.exports = {
       "black": "#09090a",
       "white": "#e9e6eb",
       "gray": "#bebbbf",
-      "fuchsia": "#bc2ac9",
-      "cyan": "#57eff7",
-      "sea-green": "#40f5af",
+      "sky-blue": "#a7bfd9",
+      "offwhite": "#e6e4dc",
+      "wine": "#91121a",
+      "maple": "#c75c6b",
+      "petal-pink": "#f7bde8",
       "lilac": "#b196eb",
+      "violet": "#733aec",
+      "ocean-green": "#319b8d",
+      "sea-green": "#21c6ab",
+      "cyan": "#35a4bf",
+      "dark-blue": "#2d3666",
       "lead": "#575c58",
-      "dark-blue": "#283057",
-      "lime": "#46f04b",
-      "orange": "#faad32",
-      "yellow": "#f5f533"
+      "sand": "#c3c388"
     }
   }
 }
