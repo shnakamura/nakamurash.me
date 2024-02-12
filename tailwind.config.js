58d7e45575cdd4706eefc0624477e9bf98a8e770
shnakamura <joaovtnakmura@gmail.com>
2024-02-12 01:17:10 -0300

    Resets website

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,7 @@ module.exports = {
     colors: {
       "black": "#09090a",
       "white": "#e9e6eb",
-      "gray": "#bebbbf",
-      "sky-blue": "#a7bfd9",
-      "offwhite": "#e6e4dc",
-      "wine": "#91121a",
-      "maple": "#c75c6b",
-      "petal-pink": "#f7bde8",
-      "lilac": "#b196eb",
-      "violet": "#733aec",
-      "ocean-green": "#319b8d",
-      "sea-green": "#21c6ab",
-      "cyan": "#35a4bf",
-      "dark-blue": "#2d3666",
-      "lead": "#575c58",
-      "sand": "#c3c388",
-      "original": "#706873"
+      "gray": "#99959c"
     }
   }
 }
